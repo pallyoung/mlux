54ae9515402c6eed6e94ccfc3b3eb1eae1757d19
yeshaopeng <yeshaopeng@liqnqin360.com>
2016-10-29 15:52:42 +0800

    0.1

--- src/index.js
+++ src/index.js
@@ -8,9 +8,26 @@ var dispatcher = new Dispatcher();
 var cm = new ControllerManager();
 var storeManager = new StoreManager();
 
-
+storeManager.actions = {};
+storeManager.handleAction = function (action, data) {
+    var actionHanler = storeManager.actions[action];
+    if (actionHanler) {
+        actionHanler(data);
+    }
+}
+storeManager.addAction = function (action, actionHanler) {
+    storeManager.actions[action] = actionHanler;
+}
+storeManager.addActions = function (actionMap) {
+    for (let action in actionMap) {
+        storeManager.actions[action] = actionMap[action];
+    }
+}
+dispatcher.register(function (payload) {
+    storeManager.handleAction(payload.action, payload.data);
+});
 export default {
-    Dispatcher:dispatcher,
-    ControllerManager:cm,
-    StoreManager:storeManager
+    Dispatcher: dispatcher,
+    ControllerManager: cm,
+    StoreManager: storeManager
 }
