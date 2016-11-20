da15e664dfccd9a17177e1c0e66d340a990666fc
yeshaopeng <yeshaopeng@liqnqin360.com>
2016-11-20 12:15:03 +0800

    调整

--- src/index.js
+++ src/index.js
@@ -3,27 +3,7 @@ import Dispatcher from './dispatcher/Dispatcher';
 import StoreManager from './store/StoreManager';
 
 import Binder from './Binder';
-var dispatcher = new Dispatcher();
-var cm = new ControllerManager();
 
-// storeManager.actions = {};
-// storeManager.handleAction = function (action, data) {
-//     var actionHanler = storeManager.actions[action];
-//     if (actionHanler) {
-//         actionHanler(data);
-//     }
-// }
-// storeManager.addAction = function (action, actionHanler) {
-//     storeManager.actions[action] = actionHanler;
-// }
-// storeManager.addActions = function (actionMap) {
-//     for (let action in actionMap) {
-//         storeManager.actions[action] = actionMap[action];
-//     }
-// }
-// dispatcher.register(function (payload) {
-//     storeManager.handleAction(payload.action, payload.data);
-// });
 export default {
     StoreManager,
     Binder
